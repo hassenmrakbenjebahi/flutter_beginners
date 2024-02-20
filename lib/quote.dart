f06797f5d432d1dfae7d1f881229335c52e1d454
hassenmrakbenjebahi <123837615+hassenmrakbenjebahi@users.noreply.github.com>
2024-02-20 22:54:36 +0100

    construteur quote

--- lib/quote.dart
+++ lib/quote.dart
@@ -20,6 +20,6 @@ class Quote {
   // constructor with named parameters
   // & automatically assigns named arguments to class properties
 
-  Quote({ this.text, this.author });
+  Quote({ required this.text,required this.author });
 
 }
